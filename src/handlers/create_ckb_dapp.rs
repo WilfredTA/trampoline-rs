3502a8d863a4c9f6d1bade7d77e5fe8630926d5f
Tannr <tannr@hey.com>
2021-09-13 13:45:44 -0400

    Create react app from local react template instead of remote template -- useful codegen later

--- src/handlers/create_ckb_dapp.rs
+++ src/handlers/create_ckb_dapp.rs
@@ -1,13 +1,92 @@
+use crate::handlers::TEMPLATES;
+use tera::Context;
 use std::process::{self, Command, Stdio};
 use std::fs;
 use anyhow::Result;
+use std::path::{Path,PathBuf};
+use tempfile::{tempdir, tempfile};
+use std::str::FromStr;
+use std::io::Write;
+
 pub fn create(name: String, path_to_template: &str) -> Result<()> {
+    let mut dapp_path = std::env::current_dir()?;
+    dapp_path.push(name.as_str());
+    let cra_template_dir = tempdir()?;
+
+    let mut cra_template_path = PathBuf::from(cra_template_dir.path());
+    let mut path_to_template = cra_template_path.clone();
+    let mut context = Context::new();
+    cra_template_path.push("dapp_template");
+
+
+
+    for file in TEMPLATES.get_template_names() {
+        if file.starts_with("dapp/dapp_template") {
+            while !&cra_template_path.ends_with("dapp_template") {
+                cra_template_path.pop();
+            }
+            let shortened_file_name = file.strip_prefix("dapp/dapp_template/")
+                .unwrap();
+
+            let contents = TEMPLATES.render(file, &context)?;
+            create_nonexistent_dirs(&PathBuf::from_str(shortened_file_name)?, &cra_template_path)?;
+            cra_template_path = cra_template_path.join(shortened_file_name);
+
+
+
+            let err_message = format!(
+                "Error writing to {:?} with template {} in project",
+                &cra_template_path.to_str().unwrap(),
+                file,
+            );
+            let mut file = fs::File::create(&cra_template_path.as_path())?;
+            file.write(contents.as_bytes()).expect(err_message.as_str());
+            fs::write(&cra_template_path, contents).expect(err_message.as_str());
+
+        }
+    }
+    path_to_template.push("dapp_template");
+
+    write_dapp(&path_to_template, name)?;
+
+
+    Ok(())
+}
+
+fn is_file<P: AsRef<Path>>(check: P) -> bool {
+    let check = check.as_ref().to_str().unwrap();
+    check.ends_with(".ts") || check.ends_with(".js") ||
+        check.ends_with(".css") || check.ends_with(".tsx") ||
+        check.ends_with(".jsx") || check.ends_with(".svg") ||
+        check.ends_with(".md")  || check.ends_with(".html") ||
+        check.ends_with(".txt") || check.ends_with(".json") ||
+        check.ends_with("gitignore")
+}
+fn create_nonexistent_dirs(path_to: &PathBuf, base_dir: &Path) -> Result<()> {
+    let mut curr_path = PathBuf::new();
+    //curr_path.push(base_dir);
+    let mut full_path = base_dir.join(path_to.clone());
+    full_path.iter().for_each(|section| {
+
+        curr_path.push(section);
+
+        if is_file(&curr_path) {
+            return;
+        }
+        if !curr_path.exists() {
+            let err_string = format!("Unable to create subpath {}", curr_path.to_str().unwrap());
+            fs::create_dir(&curr_path).expect(err_string.as_str());
+        }
+    });
+
+    Ok(())
+}
+fn write_dapp(path_to_template: &PathBuf, name: String) -> Result<()> {
+    println!("PATH TO TEMPLATE: {:?}", path_to_template);
     let mut cra_res = Command::new("npx")
-        .args(["create-react-app", format!("{}", name).as_str(), "--template", format!("file:{}", path_to_template).as_str()])
+        .args(["create-react-app", format!("{}", name).as_str(), "--template", format!("file:{}", path_to_template.to_str().unwrap()).as_str()])
         .stdout(Stdio::inherit())
         .spawn()?
         .wait()?;
-
-
     Ok(())
 }
